34be65ecbafcfc453aef5660ff5123a0860d9d68
Akash Gopinath <akash.gopinath008@gmail.com>
2023-10-16 15:40:23 +0100

    Changed conceal level

--- lua/akash/plugins/vimtex.lua
+++ lua/akash/plugins/vimtex.lua
@@ -2,7 +2,6 @@
 -- http://manpages.ubuntu.com/manpages/trusty/man5/zathurarc.5.html
 vim.g["vimtex_view_method"] = "zathura"
 --vim.g["vimtex_quickfix_mode"] = 0
-vim.opt.conceallevel = 1
 -- Ignore mappings
 -- vim.g["vimtex_mappings_enabled"] = 0
 
@@ -23,7 +22,17 @@ vim.g["vimtex_log_ignore"] = {
 }
 
 vim.g["vimtex_context_pdf_viewer"] = "okular"
--- vim.opt.conceallevel = 1
+--set conceal level 1
+vim.opt.conceallevel = 1
+vim.opt["conceallevel"] = 1
+vim.g["conceallevel"] = 1
+vim.g["tex_conceal"] = 1
+vim.g["vimtex_conceal"] = 1
+-- Disable math conceal
+-- math = 0,
+-- Disable itemize conceal
+-- itemize = 0,
+-- Disable section conce
 -- vim.g["vimtex_compiler_method"] = "latexrun"
 
 -- vim.g['vimtex_complete_enabled'] = 1
@@ -31,4 +40,5 @@ vim.g["vimtex_context_pdf_viewer"] = "okular"
 -- vim.g['vimtex_complete_close_braces'] i= 1
 --
 vim.g["vimtex_quickfix_mode"] = 0
--- vim.g["tex_conceal"] = "abdmg"
+vim.g["tex_conceal"] = "abdmg"
+--
